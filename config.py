2da482c3aa59cdfcab44d3e5c86e135c61211202
JoeC16 <joecraig16@hotmail.co.uk>
2025-04-21 00:20:24 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///onfile.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
