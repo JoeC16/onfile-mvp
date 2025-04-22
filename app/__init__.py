30d239452150b7e199d377cbf239a7a970962036
JoeC16 <joecraig16@hotmail.co.uk>
2025-04-22 23:20:33 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,14 +8,14 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your-secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///onfile.db'  # or os.getenv("DATABASE_URL")
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///onfile.db'  # or use os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['UPLOAD_FOLDER'] = 'app/uploaded_files'
+    app.config['UPLOAD_FOLDER'] = '/app/uploaded_files'
 
     db.init_app(app)
     login_manager.init_app(app)
 
-    from app.routes.main import main  # <-- MOVE THIS HERE
+    from app.routes.main import main  # Delayed import to avoid circular reference
     app.register_blueprint(main)
 
     with app.app_context():
