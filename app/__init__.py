44cf0e384ba06597e393d480cbfb0770fa138f82
JoeC16 <joecraig16@hotmail.co.uk>
2025-04-22 23:13:15 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from app.routes.main import main  # Import Blueprint
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -9,16 +8,17 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your-secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///onfile.db'  # or os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///onfile.db'  # or os.getenv("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['UPLOAD_FOLDER'] = 'app/uploaded_files'
 
     db.init_app(app)
     login_manager.init_app(app)
 
+    from app.routes.main import main  # <-- MOVE THIS HERE
+    app.register_blueprint(main)
+
     with app.app_context():
         db.create_all()
-
-    app.register_blueprint(main)  # ✅ Registers your routes via Blueprint
 
     return app
