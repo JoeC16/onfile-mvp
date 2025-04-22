2a697d5f814e678af8e21d39c154fa2dbf65b070
JoeC16 <joecraig16@hotmail.co.uk>
2025-04-22 11:05:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,10 @@ login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=10000)
 
 @login_manager.user_loader
 def load_user(user_id):
