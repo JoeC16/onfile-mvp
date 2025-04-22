af8e58472a6c19fe82c61f9fcc0a0fb382b3ccc0
JoeC16 <joecraig16@hotmail.co.uk>
2025-04-22 23:23:47 +0100

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -5,7 +5,10 @@ from .. import login_manager
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
 
 @login_manager.user_loader
 def load_user(user_id):
