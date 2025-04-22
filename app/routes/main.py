6603e714e71a13e2eb7a591be6e64d509872e5f4
JoeC16 <joecraig16@hotmail.co.uk>
2025-04-22 17:24:34 +0100

    Update main.py

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,69 +1,57 @@
-# Flask routes go here
-from flask import render_template, redirect, url_for, request, flash, send_from_directory
-from flask_login import login_user, logout_user, login_required, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
-import os
-from ..models.user import User
-from .. import db, login_manager
-from . import main
+# app/routes/main.py
 
-@main.route('/')
-def index():
-    return redirect(url_for('main.login'))
+from flask import render_template, request, redirect, url_for, session, flash
+from app.models import db, User
 
-@main.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        if User.query.filter_by(email=email).first():
-            flash('Email already exists')
-            return redirect(url_for('main.signup'))
-        new_user = User(email=email, password=generate_password_hash(password))
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(new_user)
-        return redirect(url_for('main.dashboard'))
-    return render_template('signup.html')
+def init_routes(app):
+    @app.route('/')
+    def home():
+        return render_template('home.html')
 
-@main.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = User.query.filter_by(email=email).first()
-        if not user or not check_password_hash(user.password, password):
-            flash('Invalid login')
-            return redirect(url_for('main.login'))
-        login_user(user)
-        return redirect(url_for('main.dashboard'))
-    return render_template('login.html')
+    @app.route('/signup', methods=['GET', 'POST'])
+    def signup():
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            existing_user = User.query.filter_by(email=email).first()
 
-@main.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('main.login'))
+            if existing_user:
+                flash('Account already exists. Please log in instead.')
+                return redirect(url_for('login'))
 
-@main.route('/dashboard')
-@login_required
-def dashboard():
-    files = os.listdir('app/uploaded_files')
-    return render_template('dashboard.html', files=files)
+            new_user = User(email=email)
+            new_user.set_password(password)
+            db.session.add(new_user)
+            db.session.commit()
 
-@main.route('/upload', methods=['GET', 'POST'])
-@login_required
-def upload():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            filepath = os.path.join('app/uploaded_files', file.filename)
-            file.save(filepath)
-            flash('File uploaded')
-            return redirect(url_for('main.dashboard'))
-    return render_template('upload.html')
+            session['user_id'] = new_user.id
+            return redirect(url_for('dashboard'))
 
-@main.route('/uploads/<filename>')
-@login_required
-def uploaded_file(filename):
-    return send_from_directory('app/uploaded_files', filename)
+        return render_template('signup.html')
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            user = User.query.filter_by(email=email).first()
+
+            if user and user.check_password(password):
+                session['user_id'] = user.id
+                return redirect(url_for('dashboard'))
+            else:
+                flash('Invalid credentials. Please try again.')
+
+        return render_template('login.html')
+
+    @app.route('/logout')
+    def logout():
+        session.pop('user_id', None)
+        flash('You have been logged out.')
+        return redirect(url_for('login'))
+
+    @app.route('/dashboard')
+    def dashboard():
+        if 'user_id' not in session:
+            return redirect(url_for('login'))
+        return render_template('dashboard.html')
