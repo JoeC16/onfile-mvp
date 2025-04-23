975ae265d6a0c8eca6518f28701616085e4cb5eb
JoeC16 <joecraig16@hotmail.co.uk>
2025-04-23 11:34:49 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,7 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    login_manager.login_view = 'main.login'
 
     from app.routes.main import main
     app.register_blueprint(main)
