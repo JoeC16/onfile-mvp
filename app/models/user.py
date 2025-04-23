b5e02554e68f45bec7adfeb959a7d11f53797849
JoeC16 <joecraig16@hotmail.co.uk>
2025-04-23 11:33:45 +0100

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 from .. import db
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from .. import login_manager
 
 class User(UserMixin, db.Model):
@@ -7,8 +8,11 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+    def set_password(self, raw_password):
+        self.password = generate_password_hash(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password_hash(self.password, raw_password)
 
 @login_manager.user_loader
 def load_user(user_id):
